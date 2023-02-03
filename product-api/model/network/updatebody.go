03cf11c7cd49172e28a9b9de41475df02928d043
Richard <richard.herew@gmail.com>
2023-02-03 15:26:21 +0700

    make update body fields optional

--- product-api/model/network/updatebody.go
+++ product-api/model/network/updatebody.go
@@ -1,6 +1,6 @@
 package model
 
 type UpdateBody struct {
-	RetailPrice int  `json:"retail_price" validate:"omitempty,required,numeric,min=1,max=50000000"`
-	Status      bool `json:"status" validate:"omitempty,required"`
+	RetailPrice int  `json:"retail_price" validate:"omitempty,numeric,min=1,max=50000000"`
+	Status      bool `json:"status" validate:"omitempty"`
 }
