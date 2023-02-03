a972f07619a8b58251fe5cc5c766b469bd275315
Richard <richard.herew@gmail.com>
2023-02-03 15:12:34 +0700

    updated min value of page

--- product-api/model/network/getqueries.go
+++ product-api/model/network/getqueries.go
@@ -2,7 +2,7 @@ package model
 
 type GetQueries struct {
 	Limit       int    `validate:"omitempty,numeric,min=1,max=100"`
-	Page        int    `validate:"omitempty,numeric,min=0"`
+	Page        int    `validate:"omitempty,numeric,min=1"`
 	Sort        string `validate:"omitempty,oneof=id+desc id+asc id%20desc id%20asc retail_price%20asc retail_price+asc retail_price%20desc retail_price+desc"`
 	Name        string `validate:"omitempty"`
 	Subcategory string `validate:"omitempty"`
